fec43efe268355c494ee1c633c44b6eabfb912e8
John Phillips <johnphillipsgeo@gmail.com>
2019-04-08 07:42:46 -0500

    Initial commit

---
+++ LRSTools/IdentifyRouteLocations.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Attributes;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+
+namespace LRSTools
+{
+    internal class IdentifyRouteLocations : MapTool
+    {
+        public IdentifyRouteLocations()
+        {
+            IsSketchTool = true;
+            SketchType = SketchGeometryType.Point;
+            SketchOutputMode = SketchOutputMode.Map;
+        }
+
+        protected override Task OnToolActivateAsync(bool active)
+        {
+            System.Diagnostics.Trace.WriteLine("Tool Active");
+            //Console.WriteLine("Tool Active");
+            return base.OnToolActivateAsync(active);
+        }
+
+        protected override async Task<bool> OnSketchCompleteAsync(Geometry geometry)
+        {
+
+            var identifyResult = await QueuedTask.Run(() =>
+            {
+                var resultString = "";
+                var compoundResult = new StringBuilder();
+                string resultMeasure = "";
+
+                var mv = MapView.Active;
+                Console.WriteLine("start");
+
+                // Get features that intersect geometry object
+                //var features = mv.GetFeatures(geometry);
+
+                // layer definition for all layers
+                var lyrs = mv.Map.GetLayersAsFlattenedList().OfType<FeatureLayer>();
+                foreach (var lyr in lyrs)
+                {
+                    //var lyr = lyrs.FirstOrDefault(); //For use with single layer instead of for loop
+
+                    //Get current selection (to revert later)
+                    Selection RevertSelection = lyr.GetSelection();
+
+                    //Select features that intersect geometry
+                    mv.SelectFeatures(geometry);
+
+                    //Create a list of all OIDs in this layer (for use with stacked or intersecting linework)
+                    IReadOnlyList<long> LyrOids = lyr.GetSelection().GetObjectIDs();
+                    foreach (long oid in LyrOids)
+                    {
+                        double PrevMinM, PrevMaxM;
+                        double minM, maxM;
+
+                        //Create Edit operation 
+                        var splitFeatures = new EditOperation() { };
+
+                        //long oid = lyr.GetSelection().GetObjectIDs().FirstOrDefault();
+
+                        //Inspector for line min/max before split 
+                        var PrevMinMaxInspector = new Inspector(false);
+                        PrevMinMaxInspector.Load(lyr, oid);
+                        var PrevMinMaxShape = PrevMinMaxInspector["SHAPE"] as Polyline;
+
+                        //Error handling in case the user clicks on an area of the map with no linework
+                        if (PrevMinMaxShape == null)
+                        {
+
+                            Console.WriteLine("PrevMinMaxShapeNull");
+                            lyr.SetSelection(RevertSelection);
+                            continue;
+                            //return null;
+                        }
+
+                        //Get min/max of 'before' line and then split.  Put the min/max in a list to compare to line after split
+                        GeometryEngine.Instance.GetMinMaxM(PrevMinMaxShape, out PrevMinM, out PrevMaxM);
+                        List<double> prevMeasures = new List<double> { PrevMinM, PrevMaxM };
+                        splitFeatures.Split(lyr, oid, geometry);
+                        splitFeatures.Execute();
+
+                        //Create another inspector for the line after the split has occurred
+                        var MinMaxInspector = new Inspector(false);
+                        MinMaxInspector.Load(lyr, oid);
+                        var MinMaxShape = MinMaxInspector["SHAPE"] as Polyline;
+
+
+                        if (MinMaxShape == null)
+                        {
+                            Console.WriteLine("PrevMinMaxShapeNull");
+                            lyr.SetSelection(RevertSelection);
+                            continue;
+                            //return null;
+                        }
+
+                        //Get min/max of newly split line
+                        GeometryEngine.Instance.GetMinMaxM(MinMaxShape, out minM, out maxM);
+
+                        //Compare the min max of the split line to the min/max of the original line.  If the min or max matches that
+                        //of the original line, it is not the ID value.
+                        if (prevMeasures.Contains(maxM)) { resultMeasure = Convert.ToString(minM); }
+                        else { resultMeasure = Convert.ToString(maxM); };
+                        prevMeasures = null;
+
+                        //Undo the split
+                        splitFeatures.UndoAsync();
+
+                        //Remove the 'redo' operation from the operation stack
+                        var operationMgr = MapView.Active.Map.OperationManager;
+
+                        //operationMgr.FindRedoOperations()
+                        //RemoveRedoOperation();
+
+                        //Revert the selection to what was selected before the identify was run
+                        lyr.SetSelection(RevertSelection);
+
+                        resultString = string.Format("LayerName: {0}{1}OID: {2}{3}Measure: {4}{5}Minimum Measure: {6}{7}Maximum Measure: {8}{9}{10}",
+                        Convert.ToString(lyr.Name),
+                        Environment.NewLine,
+                        Convert.ToString(oid),
+                        Environment.NewLine,
+                        resultMeasure,
+                        Environment.NewLine,
+                        Convert.ToString(PrevMinM),
+                        Environment.NewLine,
+                        Convert.ToString(PrevMaxM),
+                        Environment.NewLine,
+                        Environment.NewLine
+                        );
+
+                        compoundResult.Append(resultString.ToString());
+
+
+                    }
+
+                }
+
+                return compoundResult.ToString();
+                //return resultString.ToString();
+
+            });
+
+            if (identifyResult != null)
+            {
+                MessageBox.Show(identifyResult);
+            }
+            else
+            {
+                MessageBox.Show("null");
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LRSTools/SetFromToViewModel.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Editing.Attributes;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using ArcGIS.Desktop.Mapping.Events;
+
+
+namespace LRSTools
+{
+    internal class SetFromToViewModel : DockPane
+    {
+        private const string _dockPaneID = "LRSTools_SetFromTo";
+
+        //Icommand is bound to the ApplyMeasures button in the .xaml file
+        public ICommand ApplyMeasures { get; set; }
+
+        protected SetFromToViewModel() { 
+
+        //Subscribe to selection change in the map
+        MapSelectionChangedEvent.Subscribe(OnSelectionChanged);
+          
+            //Relay Command executes set from/to measures function
+            ApplyMeasures = new RelayCommand(() =>
+                {
+
+            //Get selected layer
+            var map = MapView.Active.Map;
+            FeatureLayer featurelayer = map.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(l => l.ShapeType == ArcGIS.Core.CIM.esriGeometryType.esriGeometryPolyline);
+
+            QueuedTask.Run(() =>
+            {
+                //Logic to check how many features are selected
+                if (featurelayer == null)
+                {
+                    MessageBox.Show(string.Format("No Features Selected"));
+                    return;
+                };
+
+                var featCount = featurelayer.GetSelection().GetCount();
+                if (featCount > 1)
+                {
+                    MessageBox.Show(string.Format("Please select a single feature."));
+                    return;
+                };
+
+                //Begin Edit operation
+                var editOp = new EditOperation
+                {
+                    Name = "Set From To"
+                };
+
+                //Get selection and initiate cursor object
+                var cursor = featurelayer.GetSelection().Search();
+
+                while (cursor.MoveNext())
+                {
+                    var feature = cursor.Current as Feature;
+                    var originalLine = feature.GetShape() as Polyline;
+
+                    //This is the function that actually sets the measures and interpolates
+                    Polyline outPolyline = GeometryEngine.Instance.SetAndInterpolateMsBetween(
+                        originalLine,
+                         Math.Round(Convert.ToDouble(_fromMeasure), 3),
+                         Math.Round(Convert.ToDouble(_toMeasure), 3)
+                        ) as Polyline;
+
+                    //Use original polyline object and replace it with the new polyline object
+                    editOp.Modify(featurelayer, feature.GetObjectID(), outPolyline);
+
+                }
+
+                //Execute edit operation and save the edits
+                editOp.Execute();
+                Project.Current.SaveEditsAsync();
+
+            });
+
+        });
+
+        }
+    /// Show the DockPane.
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        //Get the min/max values for the selected polyline and populate the dockpane with those values
+        private void OnSelectionChanged(MapSelectionChangedEventArgs args)
+        {
+            //new instance for active map and feature layer
+            var Mmap = MapView.Active.Map;
+            FeatureLayer Mfeaturelayer = Mmap.GetLayersAsFlattenedList().OfType<FeatureLayer>().FirstOrDefault(l => l.ShapeType == ArcGIS.Core.CIM.esriGeometryType.esriGeometryPolyline);
+
+            QueuedTask.Run(() =>
+            {
+                //logic to check if only one feature is selected
+                if (Mfeaturelayer.GetSelection().GetCount() < 1 || Mfeaturelayer.GetSelection().GetCount() > 1)
+                {
+                    FromMeasure = null;
+                    ToMeasure = null;
+                    return;
+                }
+
+                double minM, maxM;
+
+                long SelectedOIDs = Mfeaturelayer.GetSelection().GetObjectIDs().FirstOrDefault();
+
+                //Use inspector to get the shape
+                var MinMaxInspector = new Inspector();
+
+                MinMaxInspector.Load(Mfeaturelayer, SelectedOIDs);
+
+                var MinMaxShape = MinMaxInspector["SHAPE"] as Polyline;
+
+                //Main function for getting min max m values
+                GeometryEngine.Instance.GetMinMaxM(MinMaxShape, out minM, out maxM);
+
+                //set mvalue outputs to text boxes
+                FromMeasure = Convert.ToString(minM);
+                ToMeasure = Convert.ToString(maxM);
+
+            });
+        }
+
+        private string _heading = "Set From/To Measures";
+        public string _fromMeasure;
+        public string _toMeasure;
+
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+
+        //From and To measure properties are bound to the corresponding .xaml text boxes
+        public string FromMeasure
+        {
+            get { return _fromMeasure; }
+            set
+            {
+                SetProperty(ref _fromMeasure, value, () => FromMeasure);
+
+            }
+        }
+
+        public string ToMeasure
+        {
+            get { return _toMeasure; }
+            set
+            {
+                SetProperty(ref _toMeasure, value, () => ToMeasure);
+            }
+        }
+
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class SetFromTo_ShowButton : Button
+        {
+            protected override void OnClick()
+            {
+                SetFromToViewModel.Show();
+            }
+        }
+    }
